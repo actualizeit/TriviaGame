a8e2b5bd4dc5c5e961bc4d4aaf79bd908c059032
dbell <greenadvisor@gmail.com>
2019-07-31 15:03:03 -0400

    countdown timer added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,8 @@ var questions = [{
       "Raining",
       "Cloudy",
       "Windy"
-    ]
+    ],
+    "ca": "sunny"
   },
   {
     "q": "What time of day is it?",
@@ -14,7 +15,8 @@ var questions = [{
       "5",
       "6",
       "10"
-    ]
+    ],
+    "ca": "6"
   },
   {
     "q": "Whats your favourite music?",
@@ -23,7 +25,8 @@ var questions = [{
       "hip hop",
       "stuff",
       "classical"
-    ]
+    ],
+    "ca": "stuff"
   },
   {
     "q": "Which season is your favourite?",
@@ -32,7 +35,8 @@ var questions = [{
       "summer",
       "fall",
       "winter"
-    ]
+    ],
+    "ca": "winter"
   },
   {
     "q": "What colour are your eyes?",
@@ -41,28 +45,79 @@ var questions = [{
       "Raining",
       "Cloudy",
       "Windy"
-    ]
+    ],
+    "ca": "Windy"
   }
 ];
+
 console.log(questions);
 var questionTracker = [];
 var questionAmount = 1;
+var time = 30;
+var intervalId;
 
-// $("#start").click(console.log("wutt"));
+$("#start").click(console.log("wutt"));
+function reset() {
+    time = 30;
+}
+
+function countdown() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+  }
+
+  //  The decrement function.
+  function decrement() {
+
+    //  Decrease number by one.
+    time--;
+
+    //  Show the number in the #show-number tag.
+    $("#timeRemaining").html("<h4>" + time + " Seconds Remaining</h4>");
+
+
+    //  Once number hits zero...
+    if (time === 0) {
+
+      //  ...run the stop function.
+      stop();
+
+      //  Next Question and Reset Timer
+      nextQuestion();
+      reset();
+    }
+  }
 
 window.onload = function() {
-$("#start").on("click", startGame);
+$("button").on("click", nextQuestion);
+$("#timeRemaining").css("display","none");
+$("#question").css("display","none");
+$("#ans1").css("display","none");
+$("#ans2").css("display","none");
+$("#ans3").css("display","none");
+$("#ans4").css("display","none");
 
-function startGame() {
+
+function nextQuestion() {
     console.log("wut")
+    $("#start").css("display","none");
+    $("#timeRemaining").css("display","inline-block");
+    $("#question").css("display","inline-block");
+    $("#ans1").css("display","inline-block");
+    $("#ans2").css("display","inline-block");
+    $("#ans3").css("display","inline-block");
+    $("#ans4").css("display","inline-block");
+    countdown();
+
+
 // Iterate however many times
 for (var i = 0; i < questionAmount; i++) {
   // Keep creating random numbers until the number is unique
   do {
     var randomQuestion = Math.floor(Math.random() * questions.length);
-  } while (existingQuestions()){
-
-  $("#question").text(questions[randomQuestion].q);
+  } 
+  while (existingQuestions()){
+  $("#question").html("<h3>" + questions[randomQuestion].q + "</h3>");
   var qAns = questions[randomQuestion].a;
 
   for (var j = 0; j < qAns.length; j++) {
